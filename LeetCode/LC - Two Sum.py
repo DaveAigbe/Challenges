9e26d0415c0b921297704167955c515e5c6581c2
Dave Aigbe Jr <dave.aigbe@outlook.com>
2022-11-07 15:38:47 -0600

    Completed two-sum

--- LeetCode/LC - Two Sum.py
+++ LeetCode/LC - Two Sum.py
@@ -1,13 +1,32 @@
+"""
+Original implementation
 def two_sum(nums: list[int], target: int) -> list[int]:
-    sum_arr = []
+   sum_arr = []
 
-    for i, v in enumerate(nums):
-        for p, j in enumerate(nums):
-            if (v + j) == target and p != i:
-                sum_arr.append(i)
-                sum_arr.append(p)
+   for i, v in enumerate(nums):
+       for p, j in enumerate(nums):
+           if (v + j) == target and p != i:
+               sum_arr.append(i)
+               sum_arr.append(p)
 
-                return sum_arr
+               return sum_arr
 
 
 print(two_sum([-1, -2, -3, -4, -5], -8))
+"""
+
+
+def two_sum(nums: list[int], target: int) -> list[int]:
+    hashmap = {}
+
+    for i, v in enumerate(nums):
+        only_solution = target - v
+
+        if hashmap.get(only_solution) is not None:
+            return [hashmap[only_solution], i]
+
+        hashmap[v] = i
+
+
+
+print(two_sum([2,7,11,15], 9))
